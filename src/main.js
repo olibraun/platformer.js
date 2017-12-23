7ab3295eac1b04d3ce292fe0244ce6ede5493dac
olibraun <oliverbraun89@gmail.com>
2017-12-23 22:12:05 +0100

    Adjust sprite API

--- src/main.js
+++ src/main.js
@@ -4,63 +4,42 @@
 let global_graphics_scale = 3;
 
 function preload(){
-  //Load Specs
-  mariosmallSpec = loadJSON("sprites/mario/mariosmall.json");
-  ground1Spec = loadJSON("sprites/blocks/ground1.json");
-  ground2Spec = loadJSON("sprites/blocks/ground2.json");
-
   //Load Images
-  mariosmall = loadImage("sprites/mario/mariosmall.png");
-  ground1 = loadImage("sprites/blocks/ground1.png");
-  ground2 = loadImage("sprites/blocks/ground2.png");
+  img_sprites = loadImage("img/characters.gif");
+  img_tiles = loadImage("img/tiles.png");
+
+  //Load sprites
+  overworld = loadJSON("sprites/overworld.json");
 }
 
 function setup() {
-  createCanvas(16*16*global_graphics_scale,14*16*global_graphics_scale);
+  createCanvas(16*16*global_graphics_scale,15*16*global_graphics_scale);
 
-  mariosmallSheet = new SpriteSheet(mariosmall,mariosmallSpec.width,mariosmallSpec.height);
-  for(let i=0; i<mariosmallSpec.sprites.length; i++){
-    mariosmallSheet.define(mariosmallSpec.sprites[i],i,0);
-  }
+  sprites = new SpriteSheet(img_sprites,16,16);
+  tiles = new SpriteSheet(img_tiles,overworld.tileW,overworld.tileH);
 
-  ground1Sheet = new SpriteSheet(ground1,ground1Spec.width,ground1Spec.height);
-  for(let i=0; i<ground1Spec.sprites.length; i++){
-    ground1Sheet.define(ground1Spec.sprites[i],i,0);
-  }
-
-  ground2Sheet = new SpriteSheet(ground2,ground2Spec.width,ground2Spec.height);
-  for(let i=0; i<ground2Spec.sprites.length; i++){
-    ground2Sheet.define(ground2Spec.sprites[i],i,0);
-  }
+  overworld.tiles.forEach(tile => {
+    const index = tile.index;
+    tiles.define(tile.name,index[0],index[1]);
+  });
 }
 
 function draw() {
-  background(134, 206, 253);
-  mariosmallSheet.drawByIndex("jump-left",0,0);
-  mariosmallSheet.drawByIndex("left-1",1,1);
-  mariosmallSheet.drawByIndex("left-2",2,2);
-  mariosmallSheet.drawByIndex("left-1",3,3);
-  mariosmallSheet.drawByIndex("right-1",3,4);
-  mariosmallSheet.drawByIndex("jump-right",4,4);
-
-  for(let i=0; i<16; i++){
-    ground1Sheet.drawByIndex("ground1",i,12);
-    ground2Sheet.drawByIndex("ground2",i,13);
-  }
+  tiles.drawByIndex("ground",0,0);
+  tiles.drawByIndex("sky",1,1);
+  tiles.drawByIndex("chocolate",2,2);
+  tiles.drawByIndex("bricks",3,3);
 }
 
 //Globale Variablen
 //Für Bilder
-let mariosmall;
-let ground1;
-let ground2;
+let img_sprites;
+let img_tiles;
 
 //Für Specs
-let mariosmallSpec;
-let ground1Spec;
-let ground2Spec;
+let overworld;
+let underworld;
 
 //Für Spritesheets
-let mariosmallSheet;
-let ground1Sheet;
-let ground2Sheet;
+let sprites;
+let tiles;
