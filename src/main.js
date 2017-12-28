448aebd75271488b78ae1f29118396fd77d40795
olibraun <oliverbraun89@gmail.com>
2017-12-28 22:51:49 +0100

    Add level JSON

--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 Promise.all([
+  loadJSON("levels/1-1.json"),
   loadSpriteSheet("overworld")
 ])
-.then(([tiles]) => {
-  tiles.drawByIndexRange("sky",context,0,16,0,15);
-  tiles.drawByIndexRange("ground",context,0,16,13,2);
+.then(([levelSpec,tiles]) => {
+  window.levelSpec = levelSpec;
+  levelSpec.backgrounds.forEach(background => {
+    background.ranges.forEach(([x,xr,y,yr]) => {
+      tiles.drawByIndexRange(background.name,context, x,xr,y,yr);
+    });
+  });
 });
