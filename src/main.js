3072893dd0d28abba847b75501bc50df80bebc9c
olibraun <oliverbraun89@gmail.com>
2017-12-29 07:42:22 +0100

    Refactor loader into loaders.js

--- src/main.js
+++ src/main.js
@@ -3,15 +3,6 @@
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-function loadMarioSpriteSheet(){
-  return loadImage("img/characters.gif")
-  .then(image => {
-    const MarioSprite = new SpriteSheet(image,16,16);
-    MarioSprite.define("idle",276,44,16,16);
-    return MarioSprite;
-  });
-}
-
 loadLevel("1-1")
 .then((levelSpec) => Promise.all([
   levelSpec,
--- src/misc/loaders.js
+++ src/misc/loaders.js
@@ -32,6 +32,15 @@ function loadSpriteSheet(name){
   });
 }
 
+function loadMarioSpriteSheet(){
+  return loadImage("img/characters.gif")
+  .then(image => {
+    const MarioSprite = new SpriteSheet(image,16,16);
+    MarioSprite.define("idle",276,44,16,16);
+    return MarioSprite;
+  });
+}
+
 // function loadLevel(name) {
 //   return fetch(`/levels/${name}.json`)
 //   .then(r => r.json());
