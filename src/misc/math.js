0970b9156ecbc315f75dbfa08488075721d14864
olibraun <oliverbraun89@gmail.com>
2017-12-29 19:19:24 +0100

    Pomle's matrix class for grid collision

--- src/misc/math.js
+++ src/misc/math.js
@@ -8,3 +8,25 @@ class Vec2{
     this.y = y;
   }
 }
+
+class Matrix{
+  constructor(){
+    this.grid = [];
+  }
+
+  set(x,y,value){
+    if(!this.grid[x]){
+      this.grid[x] = [];
+    }
+
+    this.grid[x][y] = value;
+  }
+
+  get(x,y){
+    const col = this.grid[x];
+    if(col){
+      return col[y];
+    }
+    return undefined;
+  }
+}
