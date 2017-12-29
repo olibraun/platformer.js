1c364469166054e11c54a35a52d233d72b174f4f
olibraun <oliverbraun89@gmail.com>
2017-12-29 08:08:16 +0100

    Minor adjustment

--- src/entities.js
+++ src/entities.js
@@ -2,8 +2,6 @@ function createMario(){
   return loadMarioSpriteSheet()
   .then(marioSprites => {
   const mario = new Entity();
-  mario.pos.set(64,180);
-  mario.vel.set(2,-10);
 
   mario.draw = function drawMario(context){
     marioSprites.draw("idle",context,this.pos.x,this.pos.y);
