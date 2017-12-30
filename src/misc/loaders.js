0d64ccae9c995b21515da41b358da8cd52856632
olibraun <oliverbraun89@gmail.com>
2017-12-30 14:41:34 +0100

    Feat: Extend loadSpriteSheet to load both tiles and frames.

--- src/misc/loaders.js
+++ src/misc/loaders.js
@@ -54,10 +54,19 @@ function loadSpriteSheet(name){
   ]))
   .then(([spec,img]) => {
     const tiles = new SpriteSheet(img,spec.tileW,spec.tileH);
-    spec.tiles.forEach(tile => {
-      const index = tile.index;
-      tiles.defineTile(tile.name,index[0],index[1]);
-    });
+    if(spec.tiles){
+      spec.tiles.forEach(tile => {
+        const index = tile.index;
+        tiles.defineTile(tile.name,index[0],index[1]);
+      });
+    }
+
+    if(spec.frames){
+      spec.frames.forEach(frameSpec => {
+        tiles.define(frameSpec.name, ...frameSpec.rect);
+      });
+    }
+
     return tiles;
   });
 }
