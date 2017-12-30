7af69d0530553df7905de83fc69e2cff2f3bb673
olibraun <oliverbraun89@gmail.com>
2017-12-30 14:46:09 +0100

    Refactor: Remove unused module sprites.js [deprecated].

--- src/misc/sprites.js
+++
@@ -1,8 +0,0 @@
-function loadMarioSpriteSheet(){
-  return loadImage("img/characters.gif")
-  .then(image => {
-    const MarioSprite = new SpriteSheet(image,16,16);
-    MarioSprite.define("idle",276,44,16,16);
-    return MarioSprite;
-  });
-}
