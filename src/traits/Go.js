6077a3b55c1b37eb80a3931308bfef511118be76
olibraun <oliverbraun89@gmail.com>
2017-12-30 10:17:13 +0100

    Include Go trait in index.html. Also: minor refactor in Go trait.

--- src/traits/Go.js
+++ src/traits/Go.js
@@ -2,11 +2,11 @@ class Go extends Trait {
   constructor(){
     super('go');
 
-    this.direction = 0;
+    this.dir = 0;
     this.speed = 200;
   }
 
   update(entity,deltaTime){
-    entity.vel.x = this.speed * this.direction * deltaTime;
+    entity.vel.x = this.speed * this.dir * deltaTime;
   }
 }
