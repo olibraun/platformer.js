2264f4cfe65b4cb6aaa6b7db648aef664c203718
olibraun <oliverbraun89@gmail.com>
2017-12-30 15:01:33 +0100

    Refactor: Create anim.js-module.

--- src/entities.js
+++ src/entities.js
@@ -1,11 +1,3 @@
-function createAnim(frames, frameLen){
-  return function resolveFrame(distance){
-    const frameIndex = Math.floor(distance / frameLen) % frames.length;
-    const frameName = frames[frameIndex];
-    return frameName;
-  }
-}
-
 function createMario(){
   return loadSpriteSheet('mario')
   .then(marioSprites => {
