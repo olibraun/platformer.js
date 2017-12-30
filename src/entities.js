3703a0c17d9bc92fd936bedc01199154e4348dea
olibraun <oliverbraun89@gmail.com>
2017-12-30 14:50:02 +0100

    Feat: Create frame router for mario animations.

--- src/entities.js
+++ src/entities.js
@@ -4,12 +4,16 @@ function createMario(){
     const mario = new Entity();
     mario.size.set(14,16);
 
-    mario.draw = function drawMario(context){
-      marioSprites.draw('run-1', context, 0, 0);
-    }
-
     mario.addTrait(new Go());
     mario.addTrait(new Jump());
+
+    function routeFrame(mario){
+      return 'idle';
+    }
+
+    mario.draw = function drawMario(context){
+      marioSprites.draw(routeFrame(this), context, 0, 0);
+    }
 
     return mario;
   });
