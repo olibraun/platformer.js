b5a1d50d3c1bcdaf1f4347e1ccabd20862144891
olibraun <oliverbraun89@gmail.com>
2017-12-30 17:02:27 +0100

    Fix: Adjust behavior of input, remap keys.

--- src/misc/input.js
+++ src/misc/input.js
@@ -1,17 +1,17 @@
 function setupKeyboard(entity){
   const input = new KeyboardState();
-  input.addMapping('Space', keyState => {
+  input.addMapping('KeyP', keyState => {
     if(keyState){
       entity.jump.start();
     } else {
       entity.jump.cancel();
     }
   });
-  input.addMapping('ArrowRight', keyState => {
-    entity.go.dir = keyState;
+  input.addMapping('KeyD', keyState => {
+    entity.go.dir += keyState ? 1 : -1;
   });
-  input.addMapping('ArrowLeft', keyState => {
-    entity.go.dir = -keyState;
+  input.addMapping('KeyA', keyState => {
+    entity.go.dir += keyState ? -1 : 1;
   });
 
   return input;
