24f7a8c5e10606af97a6f0e760057d14ef5bf42d
olibraun <oliverbraun89@gmail.com>
2017-12-30 09:24:43 +0100

    checkY component.

--- src/classes/TileCollider.js
+++ src/classes/TileCollider.js
@@ -3,10 +3,25 @@ class TileCollider{
     this.tiles = new TileResolver(tileMatrix);
   }
 
-  test(entity){
+  checkY(entity){
     const match = this.tiles.matchByPosition(entity.pos.x, entity.pos.y);
-    if(match){
-      console.log('Matched tile', match, match.tile);
+    if(!match){
+      return;
     }
+
+    if(match.tile.name !== 'ground'){
+      return;
+    }
+
+    if(entity.vel.y > 0){
+      if(entity.pos.y > match.y1){
+        entity.pos.y = match.y1;
+        entity.vel.y = 0;
+      }
+    }
+  }
+
+  test(entity){
+    this.checkY(entity);
   }
 }
--- src/classes/TileResolver.js
+++ src/classes/TileResolver.js
@@ -11,8 +11,10 @@ class TileResolver{
   getByIndex(indexX,indexY){
     const tile = this.matrix.get(indexX,indexY);
     if(tile){
+      const y1 = indexY * this.tileSize;
       return {
         tile,
+        y1
       };
     }
   }
