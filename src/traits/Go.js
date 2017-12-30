b087a0175842b23d559b2a7cb8158f621184b1a9
olibraun <oliverbraun89@gmail.com>
2017-12-30 17:47:58 +0100

    Feat: Add decleration property to Go trait.

--- src/traits/Go.js
+++ src/traits/Go.js
@@ -4,6 +4,7 @@ class Go extends Trait {
 
     this.dir = 0;
     this.acceleration = 400;
+    this.deceleration = 300;
     this.dragFactor = 1/5000;
 
     this.distance = 0;
@@ -12,10 +13,13 @@ class Go extends Trait {
 
   update(entity,deltaTime){
     const absX = Math.abs(entity.vel.x);
-    if(this.dir){
+    if(this.dir !== 0){
       entity.vel.x += this.acceleration * deltaTime * this.dir;
       this.heading = this.dir;
       this.distance += absX * deltaTime;
+    } else if(entity.vel.x !== 0) {
+      const decel = Math.min(absX, this.deceleration * deltaTime);
+      entity.vel.x += entity.vel.x > 0 ? -decel : decel;
     } else {
       this.distance = 0;
     }
