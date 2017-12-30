cbbce847673efe8940deef80bf1109db9054a2cf
olibraun <oliverbraun89@gmail.com>
2017-12-30 09:51:16 +0100

    Add a size to the Entity class and to the mario entity.

--- src/entities.js
+++ src/entities.js
@@ -2,6 +2,7 @@ function createMario(){
   return loadMarioSpriteSheet()
   .then(marioSprites => {
   const mario = new Entity();
+  mario.size.set(14,16);
 
   mario.draw = function drawMario(context){
     marioSprites.draw("idle",context,this.pos.x,this.pos.y);
