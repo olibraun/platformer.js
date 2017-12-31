68bfb69a68f0567254cca37b41ed013c2e4284ac
olibraun <oliverbraun89@gmail.com>
2017-12-31 08:54:46 +0100

    Chore: Proof of concept for synchronous spawning.

--- src/entities/Mario.js
+++ src/entities/Mario.js
@@ -4,37 +4,39 @@ const FAST_DRAG = 1/5000;
 function loadMario(){
   return loadSpriteSheet('mario')
   .then(marioSprites => {
-    const mario = new Entity();
-    mario.size.set(14,16);
+    return function createMario() {
+      const mario = new Entity();
+      mario.size.set(14,16);
 
-    mario.addTrait(new Go());
-    mario.go.dragFactor = SLOW_DRAG;
-    
-    mario.addTrait(new Jump());
+      mario.addTrait(new Go());
+      mario.go.dragFactor = SLOW_DRAG;
+      
+      mario.addTrait(new Jump());
 
-    mario.turbo = function setTurboState(turboOn) {
-      this.go.dragFactor = turboOn ? FAST_DRAG : SLOW_DRAG;
-    }
-
-    const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 7);
-
-    function routeFrame(mario){
-      if(mario.jump.falling) {
-        return 'jump';
+      mario.turbo = function setTurboState(turboOn) {
+        this.go.dragFactor = turboOn ? FAST_DRAG : SLOW_DRAG;
       }
-      if(mario.go.distance > 0) {
-        if(mario.vel.x > 0 && mario.go.dir < 0 || mario.vel.x < 0 && mario.go.dir > 0){
-          return "brake";
+
+      const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 7);
+
+      function routeFrame(mario){
+        if(mario.jump.falling) {
+          return 'jump';
         }
-        return runAnim(mario.go.distance);
+        if(mario.go.distance > 0) {
+          if(mario.vel.x > 0 && mario.go.dir < 0 || mario.vel.x < 0 && mario.go.dir > 0){
+            return "brake";
+          }
+          return runAnim(mario.go.distance);
+        }
+        return 'idle';
       }
-      return 'idle';
-    }
 
-    mario.draw = function drawMario(context){
-      marioSprites.draw(routeFrame(this), context, 0, 0, this.go.heading < 0);
-    }
+      mario.draw = function drawMario(context){
+        marioSprites.draw(routeFrame(this), context, 0, 0, this.go.heading < 0);
+      }
 
-    return mario;
+      return mario;
+    }
   });
 }
