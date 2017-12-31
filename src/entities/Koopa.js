1cdaa3557c7b752034ebd07a21424d46e1379731
olibraun <oliverbraun89@gmail.com>
2017-12-31 13:55:42 +0100

    Feat: Create and animate panic.

--- src/entities/Koopa.js
+++ src/entities/Koopa.js
@@ -5,13 +5,17 @@ function loadKoopa() {
 
 const STATE_WALKING = Symbol('walking');
 const STATE_HIDING = Symbol('hiding');
+const STATE_PANIC = Symbol('panic');
 
 class KoopaBehavior extends Trait {
   constructor() {
     super('behavior');
-    this.state = STATE_WALKING;
+
     this.hideTime = 0;
     this.hideDuration = 5;
+    this.panicSpeed = 300;
+
+    this.state = STATE_WALKING;
   }
 
   collides(us, them) {
@@ -23,9 +27,18 @@ class KoopaBehavior extends Trait {
       if (them.vel.y > us.vel.y) {
         this.handleStomp(us, them);
       } else {
-        them.killable.kill();
+        this.handleNudge(us, them);
       }
     }
+  }
+
+  handleNudge(us, them) {
+    if(us.state === STATE_WALKING) {
+      them.killable.kill();
+    } else {
+      this.panic(us, them);
+    }
+
   }
 
   handleStomp(us, them){
@@ -35,6 +48,8 @@ class KoopaBehavior extends Trait {
       us.killable.kill();
       us.vel.set(100, -200);
       us.canCollide = false;
+    } else if (this.state === STATE_PANIC) {
+      this.hide(us);
     }
   }
 
@@ -48,6 +63,12 @@ class KoopaBehavior extends Trait {
   unhide(us) {
     us.pendulumWalk.enabled = true;
     this.state = STATE_WALKING;
+  }
+
+  panic(us, them) {
+    us.pendulumWalk.enabled = true;
+    us.pendulumWalk.speed = this.panicSpeed * Math.sign(them.vel.x);
+    this.state = STATE_PANIC;
   }
 
   update(us, deltaTime) {
@@ -64,7 +85,7 @@ function createKoopaFactory(sprite) {
   const walkAnim = sprite.animations.get('walk');
 
   function routeAnim(koopa) {
-    if(koopa.behavior.state === STATE_HIDING) {
+    if(koopa.behavior.state === STATE_HIDING || koopa.behavior.state === STATE_PANIC) {
       return 'hiding';
     }
     return walkAnim(koopa.lifetime);
