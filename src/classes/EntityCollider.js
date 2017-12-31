d5e338ba86ab03d69b59884caef07405eaeaea8d
olibraun <oliverbraun89@gmail.com>
2017-12-31 11:41:52 +0100

    Feat: Set up first draft of entity collision check.

--- src/classes/EntityCollider.js
+++ src/classes/EntityCollider.js
@@ -4,6 +4,15 @@ class EntityCollider {
   }
 
   check(subject) {
-    
+    this.entities.forEach(candidate => {
+      if (subject === candidate) {
+        return;
+      }
+
+      if (subject.bounds.overlaps(candidate.bounds)) {
+        subject.collides(candidate);
+        candidate.collides(subject);
+      }
+    })
   }
 }
