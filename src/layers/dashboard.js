d518df444c286e0b522a1fc96543dc7e194d882f
olibraun <oliverbraun89@gmail.com>
2018-01-01 13:53:10 +0100

    Feat: Dispaly coins in dashboard.

--- src/layers/dashboard.js
+++ src/layers/dashboard.js
@@ -3,6 +3,7 @@ function createDashboardLayer(font, playerEnv){
   const LINE2 = font.size * 2;
 
   const score = 24500;
+  const coins = 13;
   
   return function drawDashboard(context){
     const time = playerEnv.playerController.time;
@@ -10,7 +11,10 @@ function createDashboardLayer(font, playerEnv){
     font.print('MARIO', context, 16, LINE1);
     font.print(score.toString().padStart(6, '0'), context, 16, LINE2);
 
+    font.print('@x' + coins.toString().padStart(2, '0'), context, 96, LINE2);
+
     font.print('WORLD', context, 152, LINE1);
+    font.print('1-1', context, 160, LINE2);
 
     font.print('TIME', context, 208, LINE1);
     font.print(time.toFixed().toString().padStart(3, '0'), context, 216, LINE2);
