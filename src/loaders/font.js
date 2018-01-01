c7066296a988704c26d539e44030668ac84bebcb
olibraun <oliverbraun89@gmail.com>
2018-01-01 13:31:17 +0100

    Feat: Load all characters from the image.

--- src/loaders/font.js
+++ src/loaders/font.js
@@ -1,9 +1,19 @@
+const CHARS = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+
 function loadFont() {
   return loadImage('img/font.png')
   .then(image => {
     const fontSprite = new SpriteSheet(image);
 
-    fontSprite.define('A', 8, 16, 8, 8);
+    const size = 8;
+    const rowLen = image.width;
+
+    for (let [index,char] of [...CHARS].entries()) {
+      const x = index * size % rowLen;
+      const y = Math.floor(index * size / rowLen) * size;
+      console.log(index, char, x, y);
+      fontSprite.define(char, x, y, size, size);
+    }
 
     return fontSprite;
   });
